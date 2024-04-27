b0bf09ed4cd0ba1189eeb55312e6fc7e578088e2
Shigeru Morifuku <morifuku@gmail.com>
2024-04-27 09:49:19 +0900

    auto commit

---
+++ autocommit.sh
@@ -0,0 +1,32 @@
+cd /Volumes/2000GB/Stage/wiki
+git pull origin
+rm -rf docs
+cp -r ../Blog/mkdocs/site docs
+git add .
+git commit -m "auto commit"
+git push backup
+git push -u origin
+exit
+
+
+
+
+
+cd /Volumes/2000GB/Work/Gist
+echo ">GIT PULL"
+git pull
+echo ">GIT ADD"
+git add .
+echo ">GIT COMMIT"
+git commit -m "auto commit"
+echo ">GIT PUSH"
+git push
+exit
+
+# Initialize
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:morifuku/Gist.git
+git push -u origin main
